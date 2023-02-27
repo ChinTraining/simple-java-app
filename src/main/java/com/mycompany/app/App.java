94fce97a7d634ea21a41ffbe74d4caa8f75322ad
Chin <wyce86@gmail.com>
2023-02-27 08:35:27 +0000

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("This workshop is interesting!");
+        System.out.println("This workshop is going to end in 30 min time.");
     }
 
     public String getMessage() {
